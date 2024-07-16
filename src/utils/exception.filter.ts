fcbacceb80e99795538d06b6a0b4edf22e2ce3b3
ahmad <ahmad@io.com>
2024-07-16 18:11:19 +0300

    adding another way to translate the errors ..instead of using i18n

--- src/utils/exception.filter.ts
+++ src/utils/exception.filter.ts
@@ -1,50 +1,4 @@
-// import {
-//   ExceptionFilter,
-//   Catch,
-//   ArgumentsHost,
-//   HttpException,
-//   Logger,
-// } from '@nestjs/common';
-// import { GqlExceptionFilter, GqlArgumentsHost } from '@nestjs/graphql';
-// import { ApolloError } from 'apollo-server-errors';
-// import { I18nService } from 'nestjs-i18n';
-
-// @Catch()
-// export class AllExceptionsFilter implements GqlExceptionFilter {
-//   private readonly logger = new Logger(AllExceptionsFilter.name);
-//   constructor(private readonly i18n: I18nService) {}
-//   catch(exception: unknown, host: ArgumentsHost) {
-//     const gqlHost = GqlArgumentsHost.create(host);
-//     const context = gqlHost.getContext();
-//     const response = gqlHost.getContext().res;
-//     const request = gqlHost.getContext().req;
-
-//     let status = 500;
-//     let message = 'Internal server error';
-
-//     if (exception instanceof HttpException) {
-//       status = exception.getStatus();
-//       const res = exception.getResponse();
-//       message = typeof res === 'string' ? res : (res as any).message;
-//     }
-
-//     this.logger.warn(`Status: ${status}, Error: ${JSON.stringify(exception)}`);
-
-//     // Return a GraphQL-friendly error without exposing the stack trace
-//     return new ApolloError(message, `${status}`, {
-//       timestamp: new Date().toISOString(),
-//       path: request ? request.url : null,
-//       code: `${status}`,
-//     });
-//   }
-// }
-import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException,
-  Logger,
-} from '@nestjs/common';
+import { Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
 import { GqlExceptionFilter, GqlArgumentsHost } from '@nestjs/graphql';
 import { ApolloError } from 'apollo-server-errors';
 import { I18nContext, I18nService } from 'nestjs-i18n';
@@ -64,31 +18,23 @@ export class AllExceptionsFilter implements GqlExceptionFilter {
     let status = 500;
     let message = 'Internal server error';
     const langFromHeader = request.headers['lang'];
-    // console.log(langFromHeader);
 
     if (exception instanceof HttpException) {
       status = exception.getStatus();
       const res = exception.getResponse();
       let defaultMessage = typeof res === 'string' ? res : (res as any).message;
-      // console.log(
-      //   'message before translation: ',
-      //   (defaultMessage as string).toUpperCase(),
-      // );
 
       message = await this.i18n.t(
-        `test.HTTP_${(defaultMessage as string).toUpperCase()}`,
+        `test.HTTP_${(defaultMessage as string).toUpperCase().split(' ').join('_')}`,
         {
           lang: langFromHeader,
         },
       );
-      // console.log('after : ', message);
     } else {
       message = await this.i18n.t('test.HTTP_INTERNAL_SERVER_ERROR', {
         lang: langFromHeader,
       });
     }
-
-    // this.logger.warn(`Status: ${status}, Error: ${JSON.stringify(exception)}`);
 
     return new ApolloError(message, `${status}`, {
       timestamp: new Date().toISOString(),
@@ -97,3 +43,45 @@ export class AllExceptionsFilter implements GqlExceptionFilter {
     });
   }
 }
+
+//?without using i18n..
+// import { Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
+// import { GqlExceptionFilter, GqlArgumentsHost } from '@nestjs/graphql';
+// import { ApolloError } from 'apollo-server-errors';
+// import { translation } from 'src/translation';
+
+// @Catch()
+// export class AllExceptionsFilter implements GqlExceptionFilter {
+//   private readonly logger = new Logger(AllExceptionsFilter.name);
+//   async catch(exception: unknown, host: ArgumentsHost) {
+//     const gqlHost = GqlArgumentsHost.create(host);
+//     const context = gqlHost.getContext();
+//     const response = gqlHost.getContext().res;
+//     const request = gqlHost.getContext().req;
+
+//     const langFromHeader = request.headers['lang'];
+
+//     let status = 500;
+//     let message = 'Internal server error';
+
+//     if (exception instanceof HttpException) {
+//       status = exception.getStatus();
+//       const res = exception.getResponse();
+//       let defaultMessage = typeof res === 'string' ? res : (res as any).message;
+//       defaultMessage = (defaultMessage as string)
+//         .toUpperCase()
+//         .split(' ')
+//         .join('_');
+
+//       console.log(defaultMessage);
+//       message = translation[langFromHeader][defaultMessage];
+//     } else {
+//       message = translation[langFromHeader].INTERNAL_SERVER_ERROR;
+//     }
+//     return new ApolloError(message, `${status}`, {
+//       timestamp: new Date().toISOString(),
+//       path: request ? request.url : null,
+//       code: `${status}`,
+//     });
+//   }
+// }
